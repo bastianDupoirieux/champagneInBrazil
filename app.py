20ea63e79a29af1c989e3f4c4d2899efc902ecae
bastianDupoirieux <bast.dupo@gmail.com>
2024-06-27 21:09:42 +0200

    started developing the app to enter product information. html in templates folder, css in static folder, app.py to develop file

---
+++ app.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 27 17:09:55 2024
+
+@author: bastd
+"""
+
+from flask import Flask, render_template, request, redirect, url_for
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route('/')
+def form():
+    return render_template('index.html')
+
+@app.route('/wine_data', methods=['POST'])
+def submit_form():
+    name = request.form['name']
+    domain = request.form['domain']
+    producer = request.form['producer']
+    vintage = request.form['vintage']
+    date_bought = request.form['date_bought']
+
+    # Create a DataFrame and append it to a CSV file
+    df = pd.DataFrame([[name, domain, producer, vintage, date_bought]],
+                      columns=['Name', 'Domaine', 'Producer', 'Vintage', 'Date Bought'])
+    df.to_csv('wine_data.csv', mode='a', header=not pd.io.common.file_exists('wine_data.csv'), index=False)
+
+    return redirect(url_for('form'))
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
