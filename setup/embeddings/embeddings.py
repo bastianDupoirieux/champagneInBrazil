8de2ed70f1c016464c6f1edacaab4f5f1c014b17
bastianDupoirieux <bast.dupo@gmail.com>
2025-12-21 23:55:11 +0100

    embeddings for vector-db entrypoint

--- setup/embeddings/embeddings.py
+++ setup/embeddings/embeddings.py
@@ -1,60 +1,29 @@
-from setup.data.pdf_reader import prepare_text_from_pdf_file
-from transformers import AutoTokenizer, AutoModel
+import yaml
+from setup.embeddings import appellation_documents
+from setup.utils.embeddings import generate_embeddings
+from chromadb import EmbeddingFunction, Embeddings, Documents, Client
 import os
-import utils
 
-#Entrypoint: a dictionary per appellation containing the different elements from the rulebook
-# For every appellation, I want to tokenise and chunk the document before calculating vector embeddings
-# Step 1. Tokenise text
-# Step 2. Rechunk the text if necessary (aka more than the token limit set)
-# Step 3. Calculate the embeddings
+print(os.path.dirname(os.path.abspath(__file__)))
+with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config.yml'), 'r') as stream:
+    config = yaml.safe_load(stream)
 
-def embeddings_entrypoint(appellation_dict:dict, tokenizer:str, max_length:int): #This function might not be at the right place here
-    """
+tokenizer_name = config['tokenizer']
+model_name = config['model']
+max_tokens = config["max_tokens"]
+docs_folder = config["docs_folder"]
 
-    :param max_length:
-    :param appellation_dict:
-    :param tokenizer:
-    :return:
-    """
-    embeddings_dict = {}
-    for appellation in appellation_dict.keys():
-        if appellation not in embeddings_dict.keys():
-            prechunked_rulebook = appellation_dict[appellation]
-            for section in prechunked_rulebook:
-                for subsection in section:
-                    text = appellation + subsection
-                    if len(text) > max_length:
-                        rechunked_text = embeddings_utils.rechunk_text()
-                    embeddings_utils = EmbeddingsUtils(text, max_length)
-                    tokenised_text = embeddings_utils.tokenise_text(tokenizer)
-                    embeddings = embeddings_utils.compute_embeddings()
-            embeddings_dict[appellation] = embeddings
-        else:
-            pass
-    return embeddings_dict
+class MultilingualEmbeddingsForAppellations(EmbeddingFunction):
+    def __call__(self, texts: Documents) -> Embeddings:
+        return list(map(lambda doc: generate_embeddings(doc, tokenizer_name, model_name, max_tokens), texts))
 
 
+def main():
+    chroma_client = Client()
+    collection = chroma_client.get_or_create_collection(name=config["collection_name"],
+                                                        embedding_function = MultilingualEmbeddingsForAppellations())
 
-class EmbeddingsUtils:
+    docs_dict = appellation_documents.main(docs_folder, max_tokens)
 
-    def __init__(self, text: str, max_length: int):
-        self.text = text
-        self.max_length = max_length
+    collection.add(documents = docs_dict["documents"], ids = docs_dict["documents_ids"])
 
-    def tokenise_text(self, tokeniser):
-        pass
-
-    def rechunk_text(self) -> list:
-        """
-        Takes the text and returns a list of maximum length substrings
-        :return: a list of maximum length substrings
-        """
-        list_substr = []
-        while len(self.text) > 0:
-            list_substr.append(self.text[:self.max_length])
-
-        return list_substr
-
-    def compute_embeddings(self):
-        pass
