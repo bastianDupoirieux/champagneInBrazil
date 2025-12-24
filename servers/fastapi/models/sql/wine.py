b4580005a1861c4fbdf1fe7920152edcf475ac0e
bastianDupoirieux <bast.dupo@gmail.com>
2025-12-24 16:24:45 +0100

    refactored table to define single wine data model

--- servers/fastapi/models/sql/wine.py
+++ servers/fastapi/models/sql/wine.py
@@ -1,10 +1,9 @@
 from typing import Optional
 import uuid
-from sqlalchemy import ForeignKey
 from sqlmodel import Field, Column, JSON, SQLModel
 
-class Wines(SQLModel, table=True):
-    __tablename__ = "wines"
+class Wine(SQLModel, table=True):
+    __tablename__ = "wine"
 
     id: uuid.UUID = Field(primary_key=True, default=uuid.uuid4)
     name: str = Field(description="Name of the wine")
@@ -12,8 +11,21 @@ class Wines(SQLModel, table=True):
     region: Optional[str] = Field(description="Production region of the wine")
     country: Optional[str] = Field(description="Country of production of the wine")
     appellation: Optional[str] = Field(description="Appellation of the wine")
-    colour: str = Field(description="Colour of the wine, red, white, rosé, orange")
+    colour: str = Field(description="Colour of the wine, red, white, rosé, orange, sparkling")
     vintage: Optional[int] = Field(description="Vintage of wine production")
     grape_varieties: Optional[list[str]] = Field(description="List of grape varieties the wine is made of")
     notes: Optional[str] = Field(description="Notes of the wine", default=None)
 
+    def get_new_wine(self):
+        return Wine(
+            id = uuid.uuid4(),
+            name = self.name,
+            producer = self.producer,
+            region = self.region,
+            country = self.country,
+            appellation = self.appellation,
+            colour = self.colour,
+            vintage = self.vintage,
+            grape_varieties = self.grape_varieties,
+            notes = self.notes,
+        )
--- servers/fastapi/models/sql/wine_status.py
+++ servers/fastapi/models/sql/wine_status.py
@@ -4,6 +4,6 @@ import uuid
 class WineStatus(SQLModel, table=True):
     __tablename__ = "wine_status"
     id: uuid.UUID = Field(default = uuid.uuid4, primary_key=True)
-    wine_id: uuid.UUID = Field(foreign_key="wines.id")
+    wine_id: uuid.UUID = Field(foreign_key="wine.id")
     part_of_Cellar: bool = Field(default = True)
-    wine_has_been_drank: bool = Field(default = False)
+    wine_has_been_drunk: bool = Field(default = False)
