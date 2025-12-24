5ff22c9a52f360067567e19819b902a2227ad33b
bastianDupoirieux <bast.dupo@gmail.com>
2025-12-24 18:51:23 +0100

    added database service

---
+++ servers/backend/services/database.py
@@ -0,0 +1,43 @@
+from collections.abc import AsyncIterable
+import os
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import (
+    AsyncEngine,
+    create_async_engine,
+    async_sessionmaker,
+    AsyncSession,
+)
+from sqlmodel import SQLModel
+
+from models.sql.key_value import KeyValueModel
+from models.sql.fact_wine import FactWine
+from models.sql.wine import Wine
+from models.sql.wine_status import WineStatus
+from utils.db_utils import get_database_url_and_connect_args
+from utils.get_env import get_database_url_env
+
+database_url = get_database_url_env()
+
+database_url, connect_args = get_database_url_and_connect_args(database_url)
+
+sql_engine: AsyncEngine = create_async_engine(database_url, connect_args=connect_args)
+async_session_maker = async_sessionmaker(sql_engine, expire_on_commit=False)
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker as session:
+        yield session
+
+async def create_db_and_tables():
+    async with sql_engine.begin() as conn:
+        await conn.run_sync(
+            lambda sync_conn: SQLModel.metadata.create_all(
+                sync_conn,
+                tables = [
+                    KeyValueModel.__table__,
+                    FactWine.__table__,
+                    Wine.__table__,
+                    WineStatus.__table__,
+                ],
+            )
+        )
