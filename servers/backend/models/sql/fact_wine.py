ca00d31ef3f173a1c771e18fb00b9bffff0c9795
bastianDupoirieux <bast.dupo@gmail.com>
2025-12-24 18:50:59 +0100

    minor modification, renamed class

--- servers/backend/models/sql/fact_wine.py
+++ servers/backend/models/sql/fact_wine.py
@@ -4,7 +4,7 @@ from sqlalchemy import ForeignKey
 from sqlmodel import Field, Column, JSON, SQLModel
 import datetime
 
-class fact_wine(SQLModel, table=True):
+class FactWine(SQLModel, table=True):
     __tablename__ = "fact_wine"
     id: uuid.UUID = Field(primary_key=True, default=uuid.uuid4)
     wine_id: uuid.UUID = Field(foreign_key="wine.id")
