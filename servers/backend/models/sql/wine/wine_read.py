29b7ea252a0e681a4bea7b977e6369e1e0140edf
bastianDupoirieux <bast.dupo@gmail.com>
2025-12-29 19:24:36 +0100

    modified model for better compatibility for ceation or modification of wines

---
+++ servers/backend/models/sql/wine/wine_read.py
@@ -0,0 +1,22 @@
+import uuid
+import datetime
+from sqlmodel import SQLModel
+from typing import Optional
+
+class WineRead(SQLModel):
+    id: uuid.UUID
+
+    name: str
+    producer: str
+    region: Optional[str]
+    country: Optional[str]
+    appellation: Optional[str]
+    colour: str
+    vintage: Optional[int]
+    notes: Optional[str]
+    in_cellar: Optional[bool]
+    has_been_drunk: Optional[bool]
+    on_wishlist: Optional[bool]
+    date_bought: Optional[datetime.date]
+    price_bought: Optional[float]
+    quantity: Optional[int]
---
+++ servers/backend/models/sql/wine/wine_update.py
@@ -0,0 +1,21 @@
+from typing import Optional
+from sqlmodel import SQLModel
+import datetime
+
+class WineUpdate(SQLModel):
+    name: Optional[str] = None
+    producer: Optional[str] = None
+    region: Optional[str] = None
+    country: Optional[str] = None
+    appellation: Optional[str] = None
+    colour: Optional[str] = None
+    vintage: Optional[int] = None
+    notes: Optional[str] = None
+
+    in_cellar: Optional[bool] = None
+    has_been_drunk: Optional[bool] = None
+    on_wishlist: Optional[bool] = None
+
+    date_bought: Optional[datetime.date] = None
+    price_bought: Optional[float] = None
+    quantity: Optional[int] = None
