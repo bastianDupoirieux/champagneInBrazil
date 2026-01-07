26580e459dad045c853595dec3d4f63853584d85
bastianDupoirieux <bast.dupo@gmail.com>
2026-01-07 16:35:37 +0100

    added normalisation of ids

--- servers/frontend/services/api/wine-details.ts
+++ servers/frontend/services/api/wine-details.ts
@@ -1,24 +1,26 @@
 import { ApiResponseHandler } from "./api-error-handler";
 import type { Wine } from "../../types/wine";
+import { normaliseId } from "../../utils/ids";
 
 const endpoint = "/api/wine/{wine_id}/details";
 
 export async function fetchWineDetails(wineId: string, errorMessage?: string): Promise<Wine | null> {
+    const normalisedWineId = normaliseId(wineId);
     try {
-        const response = await fetch(endpoint.replace("{wine_id}", wineId), 
+        const response = await fetch(endpoint.replace("{wine_id}", normalisedWineId), 
         {
             method: "GET"
         }
     );
 
     if (response.status == 404) {
-        console.log(`Wine details not found for wine with ID ${wineId}`);
+        console.log(`Wine details not found for wine with ID ${normalisedWineId}`);
         return null;
     }
     const defaultError = errorMessage || `Failed to fetch wine details for wine with ID ${wineId}`;
     return await ApiResponseHandler.handleResponse(response, defaultError);
     } catch (error) {
-        console.error(`Error fetching wine details for wine with ID ${wineId}:`, error);
+        console.error(`Error fetching wine details for wine with ID ${normalisedWineId}:`, error);
         throw error;
     }
 }
