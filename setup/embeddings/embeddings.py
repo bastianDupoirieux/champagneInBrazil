c2fdffec06ce79b2abcc10fc01239d84a7738fa4
bastianDupoirieux <bast.dupo@gmail.com>
2026-01-17 16:38:31 +0100

    setup creation of collections for RAG

--- setup/embeddings/embeddings.py
+++ setup/embeddings/embeddings.py
@@ -1,10 +1,9 @@
 import yaml
 from setup.embeddings import appellation_documents
-from setup.utils.embeddings import generate_embeddings
-from chromadb import EmbeddingFunction, Embeddings, Documents, Client
+from setup.utils.embeddings import MultilingualEmbeddingFunction
+from chromadb import Client
 import os
 from dotenv import load_dotenv
-import time
 
 load_dotenv()
 HF_TOKEN = os.getenv("HF_TOKEN")
@@ -18,26 +17,31 @@ model_name = config['model']
 max_tokens = config["max_tokens"]
 docs_folder = config["docs_folder"]
 hf_timeout = config["hf_timeout"]
-
-class MultilingualEmbeddingsForAppellations(EmbeddingFunction):
-    def __call__(self, texts: Documents) -> Embeddings:
-        return list(map(lambda doc: generate_embeddings(doc, tokenizer_name, model_name, max_tokens, HF_TOKEN), texts))
+batch_size = config["BATCH_SIZE"]
 
 
 def main():
     chroma_client = Client()
     collection = chroma_client.get_or_create_collection(name=config["collection_name"],
-                                                        embedding_function = MultilingualEmbeddingsForAppellations())
+                                                        embedding_function = MultilingualEmbeddingFunction(tokenizer_name, model_name, max_tokens, HF_TOKEN, batch_size))
 
     docs_dict = appellation_documents.main(docs_folder, max_tokens)
 
-    docs_sublists = [docs_dict["documents"][i:i+1000] for i in range(0, len(docs_dict["documents"]), 1000)]
-    docs_ids_sublists = [docs_dict["documents_ids"][i:i+1000] for i in range(0, len(docs_dict["documents_ids"]), 1000)]
+    total_docs = len(docs_dict["documents"])
+    total_batches = total_docs // batch_size
 
-    for i in range(0, len(docs_sublists)): #Due to HF free tier, only 1000 requests can be made every 5 minutes.
-        # Divide the list into sublists of length 1000 to not run into a timeout error
-        docs = docs_sublists[i]
-        docs_ids = docs_ids_sublists[i]
-        collection.add(documents=docs, ids=docs_ids)
-        time.sleep(hf_timeout) # 5 minute break between all documents due to limitation from huggingface free tier
+    for batch_idx in range(total_batches):
+        print(f"Adding batch {batch_idx+1}/{total_batches}")
+        start_idx = batch_idx * batch_size
+        end_idx = min(start_idx + batch_size, total_docs)
+        docs = docs_dict["documents"][start_idx:end_idx]
+        docs_ids = docs_dict["documents_ids"][start_idx:end_idx]
 
+        try:
+            collection.add(documents=docs, ids=docs_ids)
+        except Exception as e:
+            print(f"Error adding documents: {e}")
+
+
+if __name__ == "__main__":
+    main()
