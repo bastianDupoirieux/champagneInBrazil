def05561dd4aab43129e072a7d8d3d5f4766bfd1
bastianDupoirieux <bast.dupo@gmail.com>
2026-01-18 14:03:22 +0100

    added function to call ollama model

---
+++ servers/backend/services/llm_client.py
@@ -0,0 +1,25 @@
+import ollama
+import asyncio
+from typing import List, Optional, Dict
+
+from enums.llm_provider import LLMProvider
+from models.llm_message import LLMMessage
+
+class LLMClient:
+    def __init__(self):
+        self._client = self._get_client()
+
+        self.llm_provider = "ollama"
+
+    def _get_client(self):
+        return ollama.AsyncClient()
+
+    async def generate_content(
+            self,
+            model: str,
+            messages = List[Dict[str, Any]],
+    ) -> str | None:
+        client: ollama.AsyncClient = self._client
+        response = await client.chat(model = model, messages = messages)
+
+        return response.message.content
